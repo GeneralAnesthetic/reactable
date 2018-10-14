876ccd6b00362c02bbb0afb406c74ab22c49f735
Brain <brainnovocaine@msn.com>
2018-10-13 23:22:07 -0600

    Updates

---
+++ precache-manifest.7ed310f5c4a00f87ac8b2878319cdf25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cfaef97439e8cafc760b",
+    "url": "/static/js/main.cfaef974.chunk.js"
+  },
+  {
+    "revision": "c8d15217934165f02f0d",
+    "url": "/static/js/1.c8d15217.chunk.js"
+  },
+  {
+    "revision": "cfaef97439e8cafc760b",
+    "url": "/static/css/main.39a5764b.chunk.css"
+  },
+  {
+    "revision": "ef5fe3dc27d6193bc2941714779ccee6",
+    "url": "/index.html"
+  }
+];
