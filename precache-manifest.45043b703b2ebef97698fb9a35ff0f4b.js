c6cf94cb33d81dea73b6d1acbb8540466db200da
Brain <brainnovocaine@msn.com>
2018-10-14 21:49:01 -0600

    Updates

---
+++ precache-manifest.45043b703b2ebef97698fb9a35ff0f4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61c0540cb65e51a5499a",
+    "url": "/reactable/static/js/runtime~main.61c0540c.js"
+  },
+  {
+    "revision": "e17b4786db45b7e70220",
+    "url": "/reactable/static/js/main.e17b4786.chunk.js"
+  },
+  {
+    "revision": "c8d15217934165f02f0d",
+    "url": "/reactable/static/js/1.c8d15217.chunk.js"
+  },
+  {
+    "revision": "e17b4786db45b7e70220",
+    "url": "/reactable/static/css/main.a5dcb1b4.chunk.css"
+  },
+  {
+    "revision": "58c1501cc716b2c9ecf6f949f6da9549",
+    "url": "/reactable/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36140f1f775fdf0d8120f2915536c082.js"
+  "/reactable/precache-manifest.45043b703b2ebef97698fb9a35ff0f4b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/reactable/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
